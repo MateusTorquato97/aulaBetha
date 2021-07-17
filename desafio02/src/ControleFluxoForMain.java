a7d1e5e1114e8e20f3ecf12fe99a0b0e34309fa4
MateusTorquato97 <msteus@hotmail.com>
2021-07-16 22:08:06 -0300

    'teste'

--- desafio02/src/ControleFluxoForMain.java
+++
@@ -1,7 +0,0 @@
-public class ControleFluxoForMain {
-    public static void main(String[] args) {
-        for (int x = 0; x < 1000; x++){
-            System.out.println("item: " + x);
-        }
-    }
-}
--- desafio02/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] parametros) {
-        System.out.println("Curso Java Iniciante!!");
-    }
-}
